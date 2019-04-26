87140bd6ebf72efc73005c7c8485c2eb73c565e3
evblance <evblance@users.noreply.github.com>
2019-04-26 16:24:47 +1000

    Pull data from endpoint

--- lookup.js
+++ lookup.js
@@ -1,1 +1,9 @@
 const JSON_ENDPOINT = 'https://data.nasa.gov/resource/y77d-th95.json';
+
+let asteroids = [];
+const request = fetch(JSON_ENDPOINT);
+request
+  .then(response => response.json())
+  .then((data) => {
+    asteroids = data;
+  });
